4356a0ed0b406c448b3ba041e3648b1dde15c203
SorasitWang <sorasit789@gmail.com>
2021-06-28 21:51:37 +0700

    set firebase

--- app/src/main/java/com/example/planner/login/LoginViewModel.kt
+++ app/src/main/java/com/example/planner/login/LoginViewModel.kt
@@ -1,7 +1,44 @@
 package com.example.planner.login
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class LoginViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    init{
+        val db = FirebaseFirestore.getInstance()
+        val user: MutableMap<String, Any> = HashMap()
+        user["first"] = "Ada"
+        user["middle"] = "Mathison"
+        user["last"] = "Lovelace"
+        user["born"] = 1815
+
+        // Add a new document with a generated ID
+
+        // Add a new document with a generated ID
+                db.collection("users")
+                    .add(user)
+                    .addOnSuccessListener { documentReference ->
+                        Log.d(
+                            "Firebase",
+                            "DocumentSnapshot added with ID: " + documentReference.id
+                        )
+                    }
+                    .addOnFailureListener { e -> Log.w("Firebase" "Error adding document", e) }
+
+        db.collection("users")
+            .get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    for (document in task.result) {
+                        Log.d(TAG, document.id + " => " + document.data)
+                    }
+                } else {
+                    Log.w(TAG, "Error getting documents.", task.exception)
+                }
+            }
+    }
+
 }
